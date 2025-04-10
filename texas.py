44c4b0bc3b0d528d34a312f0fd13337a482a97d5
Ortana <126905156+ortanaV2@users.noreply.github.com>
2025-04-10 16:19:07 +0200

    Update texas.py

--- texas.py
+++ texas.py
@@ -54,28 +54,43 @@ def paving_stones(size, num_stones=10, stone_spacing=32):
 
     related_points = [[point] for point in point_list]
     border_points = {i: [point] for i, point in enumerate(point_list)}
+    constant_borders = []
     while True:
         i = random.randint(0, len(point_list) - 1)
         borders = border_points[i]
         new_border_points = []
         for (bx, by) in borders:
             for nx, ny in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                if all([((bx + nx, by + ny) not in pixels_of_point) for pixels_of_point in related_points]) and 0 <= bx + nx < size and 0 <= by + ny < size:
-                    related_points[i].append((bx + nx, by + ny))
-                    new_border_points.append((bx + nx, by + ny))
+                if 0 <= bx + nx < size and 0 <= by + ny < size:
+                    if all([((bx + nx, by + ny) not in pixels_of_point) for pixels_of_point in related_points]):
+                        related_points[i].append((bx + nx, by + ny))
+                        new_border_points.append((bx + nx, by + ny))
+                    elif (bx + nx, by + ny) not in related_points[i]:
+                        constant_borders.append((bx + nx, by + ny))
         border_points[i] = new_border_points
         if all([(len(border_points[point_i]) == 0) for point_i in border_points]):
             break
     
-    colors = {i: [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)] for i in range(len(point_list))}
+    for _ in range(2):
+        new_borders = []
+        for bx, by in constant_borders:
+            for nx, ny in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                if (bx + nx, by + ny) not in constant_borders and 0 <= bx + nx < size and 0 <= by + ny < size:
+                    new_borders.append((bx + nx, by + ny))
+        constant_borders.extend(new_borders)
+    
+    # colors = {i: [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)] for i in range(len(point_list))}
     for index, point_batch in enumerate(related_points):
         for point in point_batch:
-            image[point[0], point[1]] = colors[index]
+            image[point[0], point[1]] = [0, 0, 0]
     for point in point_list:
+            image[point[0], point[1]] = [255, 255, 255]
+    for point in constant_borders:
             image[point[0], point[1]] = [255, 255, 255]
 
     img = Image.fromarray(image)
-    img.show()
+    blurred = img.filter(ImageFilter.GaussianBlur(radius=3))
+    blurred.show()
 
 random.seed()
 # splatting(size=64, num_splats=45, splat_radius_lower=3, splat_radius_upper=8, blur_radius=4)
